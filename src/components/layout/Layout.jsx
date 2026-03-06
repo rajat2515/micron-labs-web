0f1de08154b71e16494c62b63efba3fd8722ff39
Rajat Pratap Singh <rajatpratapsingh2515@gmail.com>
2026-03-06 18:49:47 +0530

    Fix: add mobile top padding so content starts below fixed header

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen font-sans">
             <Header />
-            <main className="flex-grow">
+            <main className="flex-grow pt-36 md:pt-0">
                 {children}
             </main>
             <Footer />
